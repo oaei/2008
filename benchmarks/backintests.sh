2522d18d51a4abf34ff2aa7775f2687d5f4f5dbf
Jérôme Euzenat <Jerome.Euzenat@inria.fr>
2006-06-09 21:27:19 +0000

    Initial revision

---
+++ benchmarks/backintests.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+for i in `ls -d [0-9][0-9][0-9]`
+do
+echo $i
+ed -s $i/onto.rdf << EOF &>/dev/null
+1,$ s;2006/benchmarks;tests;g
+w
+q
+EOF
+ed -s $i/refalign.rdf << EOF &>/dev/null
+1,$ s;2006/benchmarks;tests;g
+w
+q
+EOF
+ed -s $i/onto.html << EOF &>/dev/null
+1,$ s;2006/benchmarks;tests;g
+w
+q
+EOF
+ed -s $i/refalign.html << EOF &>/dev/null
+1,$ s;2006/benchmarks;tests;g
+w
+q
+EOF
+done
+for i in 206 207 210
+do
+echo $i
+ed -s $i/onto-iso8859.rdf << EOF &>/dev/null
+1,$ s;2006/benchmarks;tests;g
+w
+q
+EOF
+ed -s $i/refalign-iso8859.rdf << EOF &>/dev/null
+1,$ s;2006/benchmarks;tests;g
+w
+q
+EOF
+done
+
+for i in `ls -d [0-9][0-9][0-9]`
+do
+echo $i
+ed -s $i/refalign.rdf << EOF &>/dev/null
+1,$ s;file://localhost/Volumes/Phata/Web/html/co4/oaei/lib;http://oaei.inrialpes.fr/tests;g
+w
+q
+EOF
+ed -s $i/refalign.html << EOF &>/dev/null
+1,$ s;file://localhost/Volumes/Phata/Web/html/co4/oaei/lib;http://oaei.inrialpes.fr/tests;g
+w
+q
+EOF
+done
+for i in 206 207 210
+do
+ed -s $i/refalign-iso8859.rdf << EOF &>/dev/null
+1,$ s;file://localhost/Volumes/Phata/Web/html/co4/oaei/lib;http://oaei.inrialpes.fr/tests;g
+w
+q
+EOF
+done
