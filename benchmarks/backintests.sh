5a0fc1ad1537e812a6528a732adbaeef6479d0bc
Jérôme Euzenat <Jerome.Euzenat@inria.fr>
2008-05-25 14:39:12 +0000

    - updated for 2008

--- benchmarks/backintests.sh
+++ benchmarks/backintests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 2007 ready
+# 2008 ready
 
 echo "1) update informative part of HTML files"
 echo "2) run make.sh with CURRENT=tests"
@@ -12,7 +12,7 @@ exit
 co -l index.html
 ed index.html <<EOF
 1,$ s:\.\./\.\.:\.\.:g
-1,$ s:2007/benchmarks:tests:g
+1,$ s:2008/benchmarks:tests:g
 w
 q
 EOF
@@ -21,7 +21,7 @@ ci -u -q -m"updated test" index.html
 co -l logs.html
 ed logs.html <<EOF
 1,$ s:\.\./\.\.:\.\.:g
-1,$ s:2007/benchmarks:tests:g
+1,$ s:2008/benchmarks:tests:g
 w
 q
 EOF
@@ -30,17 +30,17 @@ ci -u -q -m"updated test" logs.html
 for d in 301 302 303 304
 do
 ed $d/refalign.rdf <<EOF
-1,$ s:2007/benchmarks:tests:g
+1,$ s:2008/benchmarks:tests:g
 w
 q
 EOF
 ed $d/refalign.html <<EOF
-1,$ s:2007/benchmarks:tests:g
+1,$ s:2008/benchmarks:tests:g
 w
 q
 EOF
 ed $d/onto.rdf <<EOF
-1,$ s:2007/benchmarks:tests:g
+1,$ s:2008/benchmarks:tests:g
 w
 q
 EOF
